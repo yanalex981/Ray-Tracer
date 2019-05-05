3afa4c1cb0ceba4587417aeea192e08f568968c3
Alex <yanalex981@users.noreply.github.com>
2019-05-05 16:48:49 -0400

    cone.o compiles

--- cone.cpp
+++ cone.cpp
@@ -2,34 +2,43 @@
 #include <cmath>
 #include <optional>
 
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/ext/matrix_transform.hpp>
+
 #include "cone.hpp"
-#include "matrix_utils.hpp"
 #include "vector.hpp"
 
 // returns the normal of the cone based on a point of intersection
-vec4d normal(const vec3d &v)
+glm::dvec4 normal(const glm::dvec3 &v)
 {
-	return roty(atan2(v.x(), v.z())) * vec4d{{ 0, 1, 1, 1 }};
+	glm::dmat4 id = glm::identity<glm::dmat4>();
+	double angle = glm::degrees(std::atan2(v.x, v.z));
+	glm::dvec3 axis = glm::dvec3{ 0, 1, 0 };
+
+	return glm::rotate(id, angle, axis) * glm::dvec4{ 0, 1, 1, 1 };
 }
 
 std::optional<hit> cone::intersect(const glm::dvec4 &ray_start, const glm::dvec4 &ray_end)
 {
 	// warp ray into model space
-	auto inv = invert(transforms);
-	auto start = cart(inv * ray_start);
-	auto end = cart(inv * ray_end);
-	auto dir = norm(end - start);
+	auto inv = glm::inverse(transforms);
+	auto start4 = inv * ray_start;
+	auto start = glm::dvec3(start4 / start4.w);
+	auto end4 = inv * ray_end;
+	auto end = glm::dvec3(end4 / end4.w);
+	auto dir = glm::normalize(end - start);
 
-	auto tip = vec3d{{ 0.0, 1.0, 0.0 }}; // tip of the cone
-	auto origin = vec3d{{ 0.0, 0.0, 0.0 }}; // origin
+	auto tip = glm::dvec3{ 0.0, 1.0, 0.0 }; // tip of the cone
+	auto origin = glm::dvec3{ 0.0, 0.0, 0.0 }; // origin
 
-	auto v = norm(origin - tip); // vec from tip going towards origin
+	auto v = glm::normalize(origin - tip); // vec from tip going towards origin
 	auto co = start - tip; // vec from tip towards ray's starting point in model space
 
 	// solutions to intersection
-	double a = std::pow(dot(dir, v), 2) - 0.5;
-	double b = 2 * ( dot(dir, v) * dot(co, v) - dot(dir, co) / 2.0 );
-	double c = std::pow(dot(co, v), 2) - dot(co, co) / 2.0;
+	double a = std::pow(glm::dot(dir, v), 2) - 0.5;
+	double b = 2 * ( glm::dot(dir, v) * glm::dot(co, v) - glm::dot(dir, co) / 2.0 );
+	double c = std::pow(glm::dot(co, v), 2) - glm::dot(co, co) / 2.0;
 
 	double discrim = b * b - 4 * a * c;
 
@@ -47,9 +56,11 @@ std::optional<hit> cone::intersect(const glm::dvec4 &ray_start, const glm::dvec4
 
 	// various vectors and dots for one intersection
 	auto i2 = start + dir * t2; // intersection on cone surface in model coords
-	auto w2 = cart(transforms * homo(i2)); // intersection in world coords
-	auto n2 = cart(dir_to_world(transforms, normal(i2))); // normal dir in world coords
-	auto dot2 = dot(i2 - tip, v); // shadow of the intersection on the axis
+	auto w24 = transforms * glm::dvec4(i2, 1); // intersection in world coords
+	auto w2 = w24 / w24.w;
+	auto n24 = dir_to_world(transforms, normal(i2)); // normal dir in world coords
+	auto n2 = n24 / n24.w;
+	auto dot2 = glm::dot(i2 - tip, v); // shadow of the intersection on the axis
 	bool d2_in_range = 0 <= dot2 && dot2 <= 1; // the shadow should be positive and less than 1
 	// >1 means the point is below the base of the cone, which is not actually part of the cone we want
 	// <0 means the point is on the shadow cone above the tip
@@ -65,9 +76,11 @@ std::optional<hit> cone::intersect(const glm::dvec4 &ray_start, const glm::dvec4
 
 	// various vectors and dots for the other intersection
 	auto i1 = start + dir * t1;
-	auto w1 = cart(transforms * homo(i1));
-	auto n1 = cart(dir_to_world(transforms, normal(i1)));
-	auto dot1 = dot(i1 - tip, v);
+	auto w14 = transforms * glm::dvec4(i1, 1);
+	auto w1 = w14 / w14.w;
+	auto n14 = dir_to_world(transforms, normal(i1));
+	auto n1 = n14 / n14.w;
+	auto dot1 = glm::dot(i1 - tip, v);
 	bool d1_in_range = 0 <= dot1 && dot1 <= 1;
 	hit hit1{ n1, w1, this };
 	if (t2 < 0) // t2 is behind the camera
